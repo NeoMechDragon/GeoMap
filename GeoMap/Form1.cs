c54f76198cb7710700898ff597580a867af055d8
NeoMechDragon <neomechdragon@gmail.com>
2017-04-01 22:27:56 +1000

    Add files via upload

--- GeoMap/Form1.cs
+++ GeoMap/Form1.cs
@@ -15,8 +15,7 @@ namespace GeoMap
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // TODO: данная строка кода позволяет загрузить данные в таблицу "sampleDatabaseDataSet.Table1". При необходимости она может быть перемещена или удалена.
-            this.table1TableAdapter.Fill(this.sampleDatabaseDataSet.Table1);
+
             string filename = @"map.html";
             string dirpath = new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.FullName;
             String strMapPath = Path.Combine(dirpath,filename);
@@ -55,12 +54,6 @@ namespace GeoMap
             }
             else
             { Data.Text = fbd.FileName; }
-            string conStr = Geotagging.Properties.Settings.Default.SampleDatabaseConnectionString;  // Строка соединения с бд
-            string sql = "INSERT INTO Table1(text) values(2)";
-            SqlConnection con = new SqlConnection(conStr);
-            SqlCommand cmd = new SqlCommand(sql, con);
-            con.Open();
-            cmd.ExecuteNonQuery();
         }
 
         private void Browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
@@ -68,22 +61,5 @@ namespace GeoMap
 
         }
 
-        private void table1BindingNavigatorSaveItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void bindingNavigatorMovePreviousItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void table1BindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
-        {
-            this.Validate();
-            this.table1BindingSource.EndEdit();
-            this.tableAdapterManager.UpdateAll(this.sampleDatabaseDataSet);
-
-        }
     }
 }
